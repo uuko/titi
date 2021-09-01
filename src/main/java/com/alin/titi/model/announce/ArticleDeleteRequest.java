0ff020bdf7dc1c2174fb2489bce0af8e385fa598
uuko <omkkqq@gmail.com>
2021-09-01 14:49:04 +0800

    " 公告文章 & banner 完成 "

---
+++ src/main/java/com/alin/titi/model/announce/ArticleDeleteRequest.java
@@ -0,0 +1,14 @@
+package com.alin.titi.model.announce;
+
+public class ArticleDeleteRequest {
+    private Integer articleId;
+    public Integer getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Integer articleId) {
+        this.articleId = articleId;
+    }
+
+
+}
