6bcda853fad69bc0c402ccb8ab8bb732e61aa9b7
uuko <omkkqq@gmail.com>
2021-09-19 23:59:07 +0800

    "put updateTeacher -> post "

--- src/main/java/com/alin/titi/controller/TeacherController.java
+++ src/main/java/com/alin/titi/controller/TeacherController.java
@@ -179,7 +179,7 @@ public class TeacherController {
     }
 
     //update
-    @PutMapping("/teacher/{tchNumber}/{tchYear}/{tchSemester}")
+    @PostMapping("/teacher/upload/{tchNumber}/{tchYear}/{tchSemester}")
     public ResponseEntity<?> update(
             @RequestBody RegisterTeacherModel putuser
             ,@PathVariable Integer tchNumber
