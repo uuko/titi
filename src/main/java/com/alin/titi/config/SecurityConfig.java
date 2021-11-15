05aff4b634c78dbbd15d7762dbde24e06bcf94c5
uuko <omkkqq@gmail.com>
2021-11-15 20:45:17 +0800

    "[new] cors test"

--- src/main/java/com/alin/titi/config/SecurityConfig.java
+++ src/main/java/com/alin/titi/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.alin.titi.config;
 
+import com.google.common.collect.ImmutableList;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,5 +29,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
         http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+
+        configuration.setAllowedOrigins(ImmutableList.of("*"));  //set access from all domains
+        configuration.setAllowedMethods(ImmutableList.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
