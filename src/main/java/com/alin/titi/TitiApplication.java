55e3aea9496df0675bb0468bd9b725a277796d48
uuko <omkkqq@gmail.com>
2021-11-15 20:57:53 +0800

    "[new] cors test"

--- src/main/java/com/alin/titi/TitiApplication.java
+++ src/main/java/com/alin/titi/TitiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication()
+@CrossOrigin(value = "http://163.17.136.180:8080")
 public class TitiApplication {
 
     public static void main(String[] args) {
